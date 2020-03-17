132181b8df949d5fed51732aa5224e16f3a7ec1c
Ángel Lozano <angel.lozano@elecnor.es>
2020-03-17 17:18:49 +0100

    change java to python, add python dummy src

--- travis/containerCreation.sh
+++ travis/containerCreation.sh
@@ -13,8 +13,8 @@ TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-0}"
 # Create a Docker image and tag it as 'travis_<build number>'
 buildTag=travis_${TRAVIS_BRANCH}_$TRAVIS_BUILD_NUMBER
 
-docker build -t eoepca/template-service .
-docker tag eoepca/template-service $DOCKER_USERNAME/template-service:$buildTag
+docker build -t $1 .
+docker tag $1 $DOCKER_USERNAME/template-service:$buildTag
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
